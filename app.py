b18d48bec4ec3f1301d212c97f4e883068cff56f
krishantha01 <t.krishanthadarmawardana@gmail.com>
2022-02-15 22:05:54 +0530

    add logging mechanism

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import flask
 from time_zone import *
+import mail
+import logging
+import watchtower
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(
+                log_group='flask-app',
+                stream_name='flask-app')
+#logger.addHandler(watchtower.CloudWatchLogHandler())
+logger.addHandler(console_handler)
+logger.addHandler(cw_handler)
+
+
+
+logger.info("application started")
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -7,4 +25,9 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return return_time()
+    try:
+        logger.info("sending the results")
+        return return_time()
+    except Exception as e:
+        logger.info("faiure in the script")
+        mail.send_mail(str(e),"Api Failure")
